101516b78cbab23e11a7933a867b6915fcb2d0b0
Jakub <kubinig@gmail.com>
2025-04-29 20:15:42 +0200

    added vl53l0x and drive_mode

---
+++ src/tof_sensor_task.cpp
@@ -0,0 +1,60 @@
+#include "tof_sensor_task.h"
+
+constexpr uint8_t VL53L0X_I2C_ADDRES = 0x29;
+
+constexpr uint8_t SENSOR_COUNT = 2;
+constexpr uint8_t XSHUT_PINS[SENSOR_COUNT] = {12, 13};
+
+
+static VL53L0X tof_sensor;
+
+static void tof_sensor_setup() {
+    //Set xshut pins low
+    for(uint8_t i=0; i<SENSOR_COUNT; i++) {
+        pinMode(XSHUT_PINS[i], OUTPUT);
+        digitalWrite(XSHUT_PINS[i], false);
+    }
+
+    tof_sensor.setAddress(VL53L0X_I2C_ADDRES);
+    if(tof_sensor.setMeasurementTimingBudget(20000) != true) {
+        Serial.println("Failed to set vl53l0x timing budget.");
+    }
+
+}
+
+
+void get_distance(uint16_t *distance) {
+    for(uint8_t i=0; i<SENSOR_COUNT; i++) {
+        digitalWrite(XSHUT_PINS[i], true); // set xshut high to read from sensor
+        distance[i] = tof_sensor.readRangeSingleMillimeters();
+        digitalWrite(XSHUT_PINS[i], false); 
+    }
+}
+
+
+void process_distance(uint16_t *distance) {
+    //sensors 1 and 2 are side sensors used to keep in track.
+    //sensor 3 is front sensor (unused)
+    if(SENSOR_COUNT==0) return;
+    if(SENSOR_COUNT>=2) {
+        //maximum difference of distance between left and right side of the patch(mm)
+        constexpr uint16_t delta_distance_toleration = 50; 
+        const int16_t delta_distance = abs(distance[0] - distance[1]);
+
+        if(delta_distance>delta_distance_toleration) {
+            //do nothing for now
+        }
+    }
+
+}
+
+
+void tof_sensor_task(__unused void *params) {
+    uint16_t distance[SENSOR_COUNT];
+    tof_sensor.readRangeSingleMillimeters();
+    while (true) {
+        if(drive_mode == DriveMode::AUTONOMOUS) {
+            get_distance(distance);
+        }
+    }
+}
